1aa794f4905e93e5a850df7fdb201f502ebf20e0
fengqiyue <mynameiszhangxu@gmail.com>
2016-06-13 22:25:00 +0800

    Connect UI to Code 完成

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,11 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+
+    // 属性
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var mealNameLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //通过代理的回调，处理文本域的用户输入
+        nameTextField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +28,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    //Action
+    @IBAction func setDefaultLabelText(sender: UIButton) {
+        mealNameLabel.text = "缺省值"
+    }
+    
+    // MARK: UITextFieldDelegate
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        //隐藏键盘
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        mealNameLabel.text = textField.text
+    }
 
 }
 
