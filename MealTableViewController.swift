bd1bfcf43c84b3cc1bb155c08feb1f4980ae2890
fengqiyue <mynameiszhangxu@gmail.com>
2016-07-10 12:53:51 +0800

    加了个用于描述的功能，似乎有点问题

--- MealTableViewController.swift
+++ MealTableViewController.swift
@@ -24,13 +24,13 @@ class MealTableViewController: UITableViewController {
     func loadSampleMeals() {
         
         let photo1 = UIImage(named: "meal1")!
-        let meal1 = Meal(name: "Caprese Salad", photo: photo1, rating: 4)!
+        let meal1 = Meal(name: "Caprese Salad", photo: photo1, rating: 4, description: "good one")!
         
         let photo2 = UIImage(named: "meal2")!
-        let meal2 = Meal(name: "Chicken and Potatoes", photo: photo2, rating: 5)!
+        let meal2 = Meal(name: "Chicken and Potatoes", photo: photo2, rating: 5,description: "delicious")!
         
         let photo3 = UIImage(named: "meal3")!
-        let meal3 = Meal(name: "Pasta with Meatballs", photo: photo3, rating: 3)!
+        let meal3 = Meal(name: "Pasta with Meatballs", photo: photo3, rating: 3,description: "还好吧")!
         
         meals += [meal1, meal2, meal3]
 
